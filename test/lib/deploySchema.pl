81d697b467512cbf434e92b22abd71b72868ea54
andreioff <andreioff@yahoo.ro>
2019-07-30 00:05:25 +0300

    Task3 & Task4

--- test/lib/deploySchema.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use lib '.';
-
-use Schema;
-
-my $connection = Schema->connect('dbi:Pg:dbname=accounts;host=localhost', 'postgres', 'postgres', {AutoCommit => 1});
-$connection->deploy();
